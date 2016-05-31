a86edd5a8c2f8b629292ad3ebd4b4687a9555b93
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-05-31 17:08:19 +0200

    sauvegarde ajouts code

---
+++ ajoutCss/situationsProHtml.php
@@ -0,0 +1,14 @@
+<div class="divLong bleu" style="margin-top:-110px" id="ancrage">
+    <div class="divCentre menuRea">
+        <ul class="menuRea">
+            <li class="menuRea"><a href="#ancrage" class="scroll" id="situation1Lien" onclick="cacherSit('situation1', 'situation2')">Situation professionnelle n°1</a></li>
+            <li class="menuRea"><a href="#ancrage" class="scroll" id="situation2Lien" onclick="cacherSit('situation2', 'situation1')">Situation professionnelle n°2</a></li>
+        </ul>
+    </div>
+<div>
+<div class="divLong2 blanc" id="situation1"> 
+    test
+</div>
+<div hidden class="divLong2 blanc" id="situation2"> 
+    test2
+</div>
