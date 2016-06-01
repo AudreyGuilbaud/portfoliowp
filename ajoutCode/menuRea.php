ae1708c72388a85f95088a27e13af17f8aa1b5ff
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-01 22:48:40 +0200

    maj pages modifiées

--- ajoutCode/menuRea.php
+++ ajoutCode/menuRea.php
@@ -1,7 +1,7 @@
 <style>
 ul.menuReaPrincip {
     list-style-type:none;
-    text-align: center; 
+    //text-align: center; 
     margin-top : 30px;
 }
 
@@ -25,45 +25,56 @@ ul li.menuReaPrincip a:hover {
 }
 
 .marge {
-    margin-top : -100px;
+    margin-top : -120px;
 }
+
+
 
 </style>
 
-<div class="divLong blanc">
-<div class="divCentre">
+
 <?php
 $url = $_SERVER['REQUEST_URI'] ;
 if (strpos($url, 'stages') !== false) { ?>
+<div class="divLong3 blanc marge">
     <ul class="menuReaPrincip">
         <li class="menuReaPrincip"><a href="index.php/realisations/situations-professionnelles"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/ppe-Copie-1-e1464394786715.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/travaux-pratiques"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/tp-Copie-Copie-1-e1464395864454.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/projets-personnels"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/projets-personnels-Copie-1-e1464395848814.jpeg"></a></li>
-    </ul> <?php
+    </ul> 
+</div>
+<?php
 }
 if (strpos($url, 'situations-professionnelles') !== false) { ?>
-    <ul class="menuRea">
+<div class="divLong3 blanc marge">
+    <ul class="menuReaPrincip">
         <li class="menuReaPrincip"><a href="index.php/realisations/stages"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/stages-Copie-1-e1464395834803.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/travaux-pratiques"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/tp-Copie-Copie-1-e1464395864454.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/projets-personnels"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/projets-personnels-Copie-1-e1464395848814.jpeg"></a></li>
-    </ul> <?php
+    </ul> 
+</div>
+<?php
 }
 if (strpos($url, 'travaux-pratiques') !== false) { ?>
-    <ul class="menuRea">
+<div class="divLong3 blanc marge">
+    <ul class="menuReaPrincip">
         <li class="menuReaPrincip"><a href="index.php/realisations/stages"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/stages-Copie-1-e1464395834803.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/situations-professionnelles"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/ppe-Copie-1-e1464394786715.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/projets-personnels"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/projets-personnels-Copie-1-e1464395848814.jpeg"></a></li>
-    </ul> <?php
+    </ul> 
+</div>
+<?php
 }
 if (strpos($url, 'projets-personnels') !== false) { ?>
-    <ul class="menuRea">
+<div class="divLong3 blanc marge">
+    <ul class="menuReaPrincip">
         <li class="menuReaPrincip"><a href="index.php/realisations/stages"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/stages-Copie-1-e1464395834803.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/situations-professionnelles"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/ppe-Copie-1-e1464394786715.jpeg"></a></li>
         <li class="menuReaPrincip"><a href="index.php/realisations/travaux-pratiques"><img src="<?php echo bloginfo('url') ;?>/wp-content/uploads/2016/05/tp-Copie-Copie-1-e1464395864454.jpeg"></a></li>
-    </ul> <?php
+    </ul> 
+</div>
+<?php
 }
 ?>
-</div>
-</div>
 
 
