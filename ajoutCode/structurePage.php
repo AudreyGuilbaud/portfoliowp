2af336dd9d52865cab1fbd42ac4f8958fb01431e
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-03 23:23:03 +0200

    fin situations professionnelles

--- ajoutCode/structurePage.php
+++ ajoutCode/structurePage.php
@@ -1,19 +1,21 @@
 <div class="divLong rouge" style="margin-top:-50px" >
     <div class="divCentre" id="ancrage" >
         <ul class="menuReaR">
-            <li style="width: 40%" class="menuReaR"><a href="#ancrage" class="scroll bouton1" onclick="cacherSit('situation1', 'situation2')"></a></li>
-            <li style="width: 40%" class="menuReaR"><a href="#ancrage" class="scroll bouton2" onclick="cacherSit('situation2', 'situation1')"></a></li>
+            <li style="width: 40%" class="menuReaR"><a href="#ancrage" class="scroll bouton1" onclick="cacherSit('situation1', 'situation2')">Situation professionnelle n°1 : PHP</a></li>
+            <li style="width: 40%" class="menuReaR"><a href="#ancrage" class="scroll bouton2" onclick="cacherSit('situation2', 'situation1')">Situation professionnelle n°2 : C#</a></li>
         </ul>
     </div>
 </div>
-
 <div class="divLong blanc" id="situation1"> 
     <div class="divLong2 blanc" id="contexte1">
         <div class="imageGauche">
-            <img src="<?php echo bloginfo('url'); ?>/#" >
+            <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/gsbSite-e1464973551678.jpg" >
         </div>
         <div class="texteDroite" >
-
+            &nbsp;&nbsp;&nbsp;&nbsp;Galaxy Swiss-Bourdin (dit GSB) est un laboratoire de fabrication de médicaments. Afin de faire connaître ses produits aux praticiens de la médecine, l’entreprise emploie des visiteurs médicaux. Ces derniers sont chargés de promouvoir les produits de l’entreprise aux médecins.
+            <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;Les visiteurs sont répartis sur tout le territoire français. Leur activité engendre des frais, que les visiteurs doivent avancer eux-mêmes. Ces frais sont ensuite remboursés par l’entreprise GSB. Pour être remboursés, les visiteurs doivent compléter, chaque mois, des fiches de frais sur le site de l’application de gestion de frais, qui renseignent l’état de leurs dépenses. 
+            <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;Les comptables de l’entreprise doivent pouvoir contrôler et valider les fiches de frais des visiteurs. Le travail réalisé complète donc l’application de gestion des frais existante, qui permettait aux visiteurs de compléter leurs fiches de frais. Cette application comprend maintenant une partie réservée aux comptables de l’entreprise.
+            <br/><br/>&nbsp;
         </div>
         <div class="divCentre bas">
             <a href="#telechargements1" class="scroll basBlancB" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
@@ -21,20 +23,40 @@
                                                                                                    onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
         </div>
     </div>
-    
-    
     <div class="divLong2 rouge" id="telechargements1">
         <div style="margin-left:6%;">
-
+            <br/>Le dossier ci-dessous contient :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les fichiers sources de l'application originelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les fichiers sources de l'application actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le fichier SQL de sauvegarde de la base de données originelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le fichier SQL de sauvegarde de la base de données actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le contexte de l'entreprise GSB ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La documentation de l'existant (spécifications, SCD...) ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La documentation de l'application actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le compte-rendu du travail réalisé ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La fiche de situation professionnelle pour l'épreuve E4 (copie de la fiche fournie dans le dossier de l'examen SANS les accès à l'application en ligne).
+            <br/>
+            <br/>
         </div>
         <div class="divCentre">
             <ul class="menuReaR">
-                <li class="menuReaR"><a href="#">Téléchargement du contenu</a></li> 
-                <li class="menuReaR"><a href="#">Lien alternatif</a></li> 
-                <li class="menuReaR"><a href="#" target="_blank">GitHub</a></li> 
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/ppe1/situationPro1GSBgestionDesFrais.zip">Téléchargement du contenu</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/gsbGestionFrais/archive/master.zip">Lien alternatif</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/gsbGestionFrais/" target="_blank">GitHub</a></li> 
             </ul>
         </div>
-
+        <div style="margin-left:6%;">
+            <br/>
+            <br/>Liens vers l'application en ligne :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les logins d'accès au site ainsi que les différents profils de tests ont été fournis dans la fiche de situation professionnelle du dossier E4.
+            <br/>
+        </div>
+        <div class="divCentre">
+            <ul class="menuReaR">
+                <li class="menuReaR"><a href="http://gsb.audreyguilbaud.fr" target="_blank">Application développée</a></li> 
+                <li class="menuReaR"><a href="http://old-gsb.audreyguilbaud.fr" target="_blank">Application originelle</a></li> 
+            </ul>
+        </div>
         <div class="divCentre">
             <a href="#contexte1" class="scroll" title="Vers Contexte"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
                                                                            onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRougeHover-e1464822716205.jpg'"
@@ -44,10 +66,30 @@
                                                                                  onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
         </div>
     </div>
-    
-    
     <div class="divLong2 bleu" id="competences1">
         <div style="margin-left : 6%">
+            <br>
+            <br/>Situations obligatoires :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Participation à un projet d'évolution d'un SI (solution applicative et d'infrastructure portant prioritairement sur le domaine de spécialité du candidat ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Prise en charge d'incidents et de démarches d'assistance liées au domaine de spécialité du candidat.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Elaboration de documents relatifs à la production et à la fourniture de services ;
+            <br/>
+            <br/>Compétences mises en oeuvres :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.1.1</b> - Analyse du cahier des charges d'un service à produire ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.3.4</b> - Déploiement d'un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.4.1</b> - Participation à un projet ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.1</b> - Identification, qualification et évaluation d'un problème ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.2</b> - Proposition d'amélioration d'un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.1</b> - Proposition d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.2</b> - Conception ou adaptation de l'interface utilisateur d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.3</b> - Conception ou adaptation d'une base de données ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.4</b> - Définition des caratéristiques d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.5</b> - Prototypage de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.6</b> - Gestion d'environnement de développement et de tests ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.7</b> - Développement, utilisation ou adaptation de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.9</b> - Rédaction d'une documentation technique ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.1</b> - Exploitation des référentiels, normes et standards adoptés par le prestataire.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; 
 
         </div>
         <div class="divCentre">
@@ -57,16 +99,17 @@
         </div>
     </div>
 </div>
-
-
-
-
 <div hidden class="divLong blanc" id="situation2"> 
     <div class="divLong2 blanc" id="contexte2">
-        <div class="imageGauche">
-            <img src="<?php echo bloginfo('url'); ?>/#" >
-        </div>
-        <div class="texteDroite" >
+        <div style="margin-top : 30px">
+            <div class="imageGauche" style="margin-right : 5%">
+                <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/console-e1464987325704.png" >
+            </div>
+            &nbsp;&nbsp;&nbsp;&nbsp;Galaxy Swiss-Bourdin (dit GSB) est un laboratoire de fabrication de médicaments. Afin de faire connaître ses produits aux praticiens de la médecine, l’entreprise emploie des visiteurs médicaux. Ces derniers sont chargés de promouvoir les produits de l’entreprise aux médecins.
+            <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;Les visiteurs sont répartis sur tout le territoire français. Leur activité engendre des frais, que les visiteurs doivent avancer eux-mêmes. Ces frais sont ensuite remboursés par l’entreprise GSB. Pour être remboursés, les visiteurs doivent compléter, chaque mois, des fiches de frais sur le site de l’application de gestion de frais, qui renseignent l’état de leurs dépenses. Les comptables contrôlent, valident et mettent en paiement les fiches des visiteurs sur cette même application Web.
+            <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;L'application développée en C# remplit deux objectifs :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Clôturer automatiquement les fiches du mois précédent dont la saisie est encore en cours afin de permettre aux comptables de les contrôler.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Mettre en paiement automatiquement les fiches du mois précédent qui ont été clôturées à partir du vingtième jour du mois actuel.
 
         </div>
         <div class="divCentre">
@@ -77,13 +120,20 @@
     </div>
     <div class="divLong2 rouge" id="telechargements2">
         <div style="margin-left:6%;">
-
+            <br/>Le dossier ci-dessous contient :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les fichiers sources de l'application réalisée ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le fichier SQL de sauvegarde de la base de données actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le contexte de l'entreprise GSB et les spécifications ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La documentation de l'application actuelle (diagramme, rapport de tests, documentation générée...)
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le compte-rendu du travail réalisé ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La fiche de situation professionnelle pour l'épreuve E4 (copie de la fiche fournie dans le dossier de l'examen).
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;
         </div>
         <div class="divCentre">
             <ul class="menuReaR">
-                <li class="menuReaR"><a href="#">Téléchargement du contenu</a></li> 
-                <li class="menuReaR"><a href="#">Lien alternatif</a></li> 
-                <li class="menuReaR"><a href="#" target="_blank">GitHub</a></li> 
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/ppe2/situationPro2GSBgestionDesEtatsDesFiches">Téléchargement du contenu</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/gsbGestionFrais/archive/master.zip">Lien alternatif</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/gsbGestionEtatFiches/" target="_blank">GitHub</a></li> 
             </ul>
         </div>
         <div class="divCentre">
@@ -95,10 +145,30 @@
                                                         onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
         </div>
     </div>
-    
+
     <div class="divLong2 bleu" id="competences2">
         <div style="margin-left : 6%">
-
+            <br>
+            <br/>Situations obligatoires :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Participation à un projet d'évolution d'un SI (solution applicative et d'infrastructure portant prioritairement sur le domaine de spécialité du candidat ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Prise en charge d'incidents et de démarches d'assistance liées au domaine de spécialité du candidat.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Elaboration de documents relatifs à la production et à la fourniture de services ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Productions relatives à la mise en place d’un dispositif de veille technologique et à l’étude d’une technologie, d’un composant, d’un outil ou d’une méthode.
+            <br/>
+            <br/>Compétences mises en oeuvres :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.1.1</b> - Analyse du cahier des charges d’un service à produire ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.2.4</b> - Détermination des tests nécessaires à la validation d’un service ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.4.1</b> - Participation à un projet ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.2.1</b> - Suivi et résolution d’incidents ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.1</b> - Identification, qualification et évaluation d’un problème ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.1</b> - Proposition d’une solution applicative ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.4</b> - Définition des caractéristiques d’une solution applicative ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.5</b> - Prototypage de composants logiciels ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.6</b> - Gestion d’environnement de développement et de tests ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.7</b> - Développement, utilisation ou adaptation de composants logiciels ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.8</b> - Réalisation des tests nécessaires à la validation d’éléments adaptés ou développés ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.9</b> - Rédaction d’une documentation technique.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;
         </div>
         <div class="divCentre">
             <a href="#telechargements2" class="scroll"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
