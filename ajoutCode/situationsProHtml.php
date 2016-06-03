8e2a72534e1965b226ea0970fa975b4749b4d0c3
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-03 20:50:41 +0200

    téléchargements et compétences situation 1

--- ajoutCode/situationsProHtml.php
+++ ajoutCode/situationsProHtml.php
@@ -18,24 +18,81 @@
             <br/><br/>&nbsp;
         </div>
         <div class="divCentre bas">
-            <a href="#telechargements1" class="scroll basBlancB"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
-                                                                      onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
-                                                                      onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+            <a href="#telechargements1" class="scroll basBlancB" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+                                                                                                   onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
+                                                                                                   onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
         </div>
     </div>
     <div class="divLong2 rouge" id="telechargements1">
+        <div style="margin-left:6%;">
+            <br/>Le dossier ci-dessous contient :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les fichiers sources de l'application originelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les fichiers sources de l'application actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le fichier SQL de sauvegarde de la base de données originelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le fichier SQL de sauvegarde de la base de données actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le contexte de l'entreprise GSB ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La documentation de l'existant (spécifications, SCD...) ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La documentation de l'application actuelle ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le compte-rendu du travail réalisé ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La fiche de situation professionnelle pour l'épreuve E4 (copie de la fiche fournie dans le dossier de l'examen SANS les accès à l'application en ligne).
+            <br/>
+            <br/>
+        </div>
         <div class="divCentre">
-            <a href="#contexte1" class="scroll"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
+            <ul class="menuReaR">
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/ppe1/situationPro1GSBgestionDesFrais.zip">Téléchargement du contenu</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/gsbGestionFrais/archive/master.zip">Lien alternatif</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/gsbGestionFrais/" target="_blank">GitHub</a></li> 
+            </ul>
+        </div>
+        <div style="margin-left:6%;">
+            <br/>
+            <br/>Liens vers l'application en ligne :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les logins d'accès au site ainsi que les différents profils de tests ont été fournis dans la fiche de situation professionnelle du dossier E4.
+            <br/>
+        </div>
+        <div class="divCentre">
+            <ul class="menuReaR">
+                <li class="menuReaR"><a href="http://gsb.audreyguilbaud.fr" target="_blank">Application développée</a></li> 
+                <li class="menuReaR"><a href="http://old-gsb.audreyguilbaud.fr" target="_blank">Application originelle</a></li> 
+            </ul>
+        </div>
+        <div class="divCentre">
+            <a href="#contexte1" class="scroll" title="Vers Contexte"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
                                                      onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRougeHover-e1464822716205.jpg'"
                                                      onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg'"></a>
-            <a href="#competences1" class="scroll"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg" 
+            <a href="#competences1" class="scroll" title="Vers Compétences"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg" 
                                                         onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRougeHover-e1464822726938.jpg'"
                                                         onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
         </div>
     </div>
     <div class="divLong2 bleu" id="competences1">
+        <div style="margin-left : 6%">
+                <br>
+                <br/>Situations obligatoires :
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Participation à un projet d'évolution d'un SI (solution applicative et d'infrastructure portant prioritairement sur le domaine de spécialité du candidat ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Prise en charge d'incidents et de démarches d'assistance liées au domaine de spécialité du candidat.
+                <br/>
+                <br/>Compétences mises en oeuvres :
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.1.1</b> - Analyse du cahier des charges d'un service à produire ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.3.4</b> - Déploiement d'un service ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.4.1</b> - Participation à un projet ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.1</b> - Identification, qualification et évaluation d'un problème ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.2</b> - Proposition d'amélioration d'un service ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.1</b> - Proposition d'une solution applicative ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.2</b> - Conception ou adaptation de l'interface utilisateur d'une solution applicative ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.3</b> - Conception ou adaptation d'une base de données ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.4</b> - Définition des caratéristiques d'une solution applicative ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.5</b> - Prototypage de composants logiciels ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.6</b> - Gestion d'environnement de développement et de tests ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.7</b> - Développement, utilisation ou adaptation de composants logiciels ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.9</b> - Rédaction d'une documentation technique ;
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.1</b> - Exploitation des référentiels, normes et standards adoptés par le prestataire.
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp; 
+                
+            </div>
         <div class="divCentre">
-            <a href="#telechargements1" class="scroll"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
+            <a href="#telechargements1" class="scroll" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
                                                             onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleuHover-e1464822751465.jpg'"
                                                             onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg'"></a>
         </div>
@@ -44,9 +101,9 @@
 <div hidden class="divLong blanc" id="situation2"> 
     <div class="divLong2 blanc" id="contexte2">
         <div class="divCentre">
-            <a href="#telechargements2" class="scroll"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
-                                                            onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
-                                                            onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+            <a href="#telechargements2" class="scroll" title="Vers téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+                                                                                         onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
+                                                                                         onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
         </div>
     </div>
     <div class="divLong2 rouge" id="telechargements2">
