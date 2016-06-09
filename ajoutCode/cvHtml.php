584a67ac0617e0afadd16664e038a65f0db836b0
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-09 22:12:22 +0200

    finalisation CV

--- ajoutCode/cvHtml.php
+++ ajoutCode/cvHtml.php
@@ -71,6 +71,9 @@
             <br>
             <br>
             <br>
+            <br>
+            <br>
+            <br>
         </div>
         <div class="divCentre bas">
             <a href="#experience" class="scroll basBlancB" title="Vers Expérience"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg" 
@@ -80,8 +83,26 @@
     </div>
     <div class="divLong2 rouge" id="experience">
         <div class="divLong2 rouge pad titre1">Expérience professionnelle</div>
-        <div style="margin-left:6%;">
-
+        <div style="margin-left:6%; margin-top:-30px ;">
+            <br>
+            <br><span class="titre3">Février 2016 : Stage de 2nd année BTS Services Informatiques aux Organisations</span>
+            <br><span>Win Novation Informatique - Orvault</span>
+            <br><span class="italique">Développement d’une partie d’une application Web de Gestion de Comité d’Entreprise en PHP à partir d’un existant en C++. 
+                                        Transfert de la base de données de Paradox à MySQL, amélioration de la modélisation, développement des parties identification, menu principal, adhérents et sociétés.</span>
+            <br>
+            <br><span class="titre3">Juin 2015 : Stage de 1ère année BTS Services Informatiques aux Organisations</span>
+            <br><span>Caisse d’Assurance Retraite et de la Santé Au Travail - Pays de la Loire - Nantes</span>
+            <br><span class="italique">Développement d’un formulaire sous Infopath 2010 et Sharepoint 2010 afin d’informatiser des demandes d’études et de statistiques. 
+                Etude du besoin - Rédaction de documentation technique et utilisateur - Développement de l’applicatif - Contact avec le client et présentation de l’applicatif.</span>
+            <br>
+            <br><span class="titre3">Cercle familial</span>
+            <br><span class="italique">Installation de matériel informatique - Dépannage des ordinateurs personnels - Gestion et dépannage du réseau - Réparation d'ordinateurs fixes - 
+                                        Diagnostic et résolution des pannes</span>
+            <br>
+            <br><span class="titre3">Cercle associatif</span>
+            <br><span class="italique">Gestion de serveurs de communication (Mumble, Teamspeak), Création et hébergement d'un serveur Teamspeak, Gestion de serveurs de jeux et installation de fonctionnalités, Administration de forums</span>
+            <br>
+            <br>
         </div>
         <div class="divCentre">
             <a href="#competences" class="scroll" title="Vers Compétences"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
@@ -94,8 +115,14 @@
     </div>
     <div class="divLong2 blanc" id="formation">
         <div class="divLong2 blanc pad titre1">Formation</div>
-        <div style="margin-left : 6%">
-
+        <div style="margin-left : 6%; margin-top:-30px ;">
+            <br>
+            <br><span class="titre3">2014-2016 : Préparation du BTS Services Informatiques aux Organisations spécialité Solutions Logicielles et Applications Métiers</span>
+            <br><span>CNED</span>
+            <br>
+            <br><span class="titre3">2013 : Baccalauréat littéraire spécialité Mathématiques</span>
+            <br><span>CNED</span>
+            <br>
 
         </div>
         <div class="divCentre">
@@ -109,9 +136,20 @@
     </div>
     <div class="divLong2 bleu" id="loisirs">
         <div class="divLong2 bleu pad titre1">Centres d'intérêt</div>
-        <div style="margin-left : 6%">
-
-
+        <div style="margin-left : 6% ; margin-top:-30px ;">
+            <br><span class="titre3">Musique</span>
+            <br><span>Ecoute, Pratique & Apprentissage</span>
+            <br><span class="italique">Classique & Soundtracks, Rock international, Celtique, Jazz, Pop</span>
+            <br><span class="italique">Musique théorique, piano & claviers, guitare</span>
+            
+            <br>
+            <br><span class="titre3">Littérature</span>
+            <br><span>Lecture & Ecriture</span>
+            <br><span class="italique">Historique, Classique, Fantastique, Fantasy, Science-fiction</span>
+            <br>
+            <br><span class="titre3">Culture japonaise</span>
+            <br><span class="italique">Musique, coutumes, langue, technologie, gastronomie, histoire</span>
+            <br>
         </div>
         <div class="divCentre">
             <a href="#formation" class="scroll" title="Vers Formation"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
@@ -123,13 +161,13 @@
         </div>
     </div>
     <div class="divLong2 rouge" id="telechargement">
-        <div class="divLong2 rouge pad titre1"></div>
+        <div class="divLong2 rouge pad titre1">Téléchargement</div>
         <div style="margin-left:6%;">
 
         </div>
         <div class="divCentre">
             <ul class="menuReaR">
-                <li class="menuReaR"><a href="">Téléchargement PDF du CV</a></li> 
+                <li class="menuReaR"><a download href="http://telechargements.audreyguilbaud.fr/CvAudreyGuilbaud.pdf">Téléchargement PDF du CV</a></li> 
             </ul>
         </div>
 
