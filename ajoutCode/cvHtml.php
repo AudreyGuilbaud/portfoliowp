fe38f2f2e8a1e7a62e3d0702d8d6ae5084a0655b
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-09 21:39:11 +0200

    partie compétences

--- ajoutCode/cvHtml.php
+++ ajoutCode/cvHtml.php
@@ -1,46 +1,35 @@
 <div class="divLong blanc" style="margin-top:-120px">
-    <div class="divLong2 blanc" id="competences">
-        <div class="divLong2 blanc pad titre1">Compétences</div>
+    <div class="divLong2 bleu" id="competences">
+        <div class="divLong2 bleu pad titre1">Compétences</div>
         <div class="texteOnlyG ">
             <span class="titre2">Langages</span>
             <br>
             <br><span class="titre3">HTML/CSS</span> 
-            <br><span class="italique">- Utilisation d'un framework front-end</span>
-            <br><span class="italique">- Réalisation de la partie front-end d'applications Web</span>
+            <br><span class="italique">Utilisation d'un framework front-end, Réalisation de la partie front-end d'applications Web</span>
             <br>
             <br><span class="titre3">PHP</span> 
-            <br><span class="italique">- Réalisation de la partie back-end d'applications Web</span>
+            <br><span class="italique">Réalisation de la partie back-end d'applications Web</span>
             <br>
             <br><span class="titre3">Javascript</span>
-            <br><span class="italique">- Utilisation du framework JQuery</span>
-            <br><span class="italique">- Utilisation d'Ajax</span>
+            <br><span class="italique">Utilisation du framework JQuery, Utilisation d'Ajax</span>
             <br>
             <br><span class="titre3">C#</span> 
-            <br><span class="italique">- Réalisation d'une application d'automatisation de requêtes en base de données sans interface</span>
-            <br><span class="italique">- Utilisation dans le cadre du développement d'un formulaire Infopath sur serveur Sharepoint</span>
+            <br><span class="italique">Réalisation d'une application d'automatisation de requêtes en base de données sans interface, Utilisation dans le cadre du développement d'un formulaire Infopath sur serveur Sharepoint</span>
             <br>
             <br><span class="titre3">Java</span>
-            <br><span class="italique">- Réalisation d'une petite application Android</span>
-            <br><span class="italique">- Réalisation d'un jeu-vidéo 2D en réseau</span>
+            <br><span class="italique">Réalisation d'une petite application Android, Réalisation d'un jeu-vidéo 2D en réseau</span>
         </div>
        <div class="texteOnlyD">
             <span class="titre2">Bases de données et modélisation</span>
             <br>
             <br><span class="titre3">SQL et SQL/PSM</span>
-            <br><span class="italique">- Création et gestion de bases de données sous PostgreSql et MySQL</span>
-            <br><span class="italique">- Utilisation de requêtes d'interrogation et d'administration</span>
-            <br><span class="italique">- Utilisation de triggers, de procédures stockées, de fonctions stockées et de curseurs</span>
+            <br><span class="italique">Création, gestion et exploitation de bases de données sous PostgreSql et MySQL, Utilisation de requêtes d'interrogation et d'administration, de triggers, de procédures stockées, de fonctions stockées et de curseurs</span>
             <br>
             <br><span class="titre3">Merise/2</span>
-            <br><span class="italique">- Création de schémas conceptuels de données à partir d'une base de données ou d'un schéma relationnel</span>
-            <br><span class="italique">- Création de schémas conceptuels de données à partir d'un contexte donné</span>
-            <br><span class="italique">- Utilisation de Win Design</span>
+            <br><span class="italique">Création de schémas conceptuels de données à partir d'une base de données, d'un schéma relationnel ou d'un contexte donné, Utilisation de Win Design</span>
             <br>
             <br><span class="titre3">UML</span>
-            <br><span class="italique">- Création de diagrammes à partir d'une application</span>
-            <br><span class="italique">- Création de diagrammes à partir d'un contexte donné</span>
-            <br><span class="italique">- Utilisation de Win Design</span>
-            <br>
+            <br><span class="italique">Création de diagrammes à partir d'une application ou d'un contexte donné, Utilisation de Win Design</span>
             <br>
             <br>
             <br>
@@ -51,14 +40,42 @@
         </div>
         <div class="texteOnlyG" style="margin-top:30px">
             <span class="titre2">Outils</span>
+            <br>
+            <br><span class="titre3">CMS</span>
+            <br><span class="italique">Drupal, Wordpress</span>
+            <br>
+            <br><span class="titre3">Versionning</span>
+            <br><span class="italique">Utilisation de Git, GitHub et Bitbucket dans de nombreux projets</span>
+            <br>
+            <br><span class="titre3">Gestion des tests</span>
+            <br><span class="italique">Utilisation de frameworks de tests unitaires, Réalisation de dossier de tests fonctionnels, Contrôle du fonctionnement d'une application</span>
+            <br>
+            <br><span class="titre3">Documentation technique et utilisateur</span>
+            <br><span class="italique">Génération de documentation automatique, Rédaction de documents techniques (spécifications fonctionnelles, dossier de sécurité), Rédaction de tutoriels</span>
+            <br>
+            
         </div>
         <div class="texteOnlyD" style="margin-top:30px">
             <span class="titre2">Langues</span>
+            <br>
+            <br><span class="titre3">Anglais</span>
+            <br><span class="italique">Niveau B2/C1, lecture de sites et de forums, visionnage de vidéos</span>
+            <br>
+            <br><span class="titre3">Japonais</span>
+            <br><span class="italique">Notions à l'oral uniquement, pratique dans le cercle amical</span>
+            <br>
+            <br><span class="titre3">Espagnol</span>
+            <br><span class="italique">Niveau B1</span>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
         </div>
         <div class="divCentre bas">
-            <a href="#experience" class="scroll basBlancB" title="Vers Expérience"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
-                                                                                        onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
-                                                                                        onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+            <a href="#experience" class="scroll basBlancB" title="Vers Expérience"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg" 
+                                                                                        onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleuHover-e1464822759168.jpg'"
+                                                                                        onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg'"></a>
         </div>
     </div>
     <div class="divLong2 rouge" id="experience">
@@ -75,34 +92,34 @@
                                                                             onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
         </div>
     </div>
-    <div class="divLong2 bleu" id="formation">
-        <div class="divLong2 bleu pad titre1">Formation</div>
+    <div class="divLong2 blanc" id="formation">
+        <div class="divLong2 blanc pad titre1">Formation</div>
         <div style="margin-left : 6%">
 
 
         </div>
         <div class="divCentre">
-            <a href="#experience" class="scroll" title="Vers Experience"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
-                                                                              onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleuHover-e1464822751465.jpg'"
-                                                                              onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg'"></a>
-            <a href="#loisirs" class="scroll" title="Vers Centres d'intérêt"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg" 
-                                                                                  onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleuHover-e1464822759168.jpg'"
-                                                                                  onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg'"></a>
-        </div>
-    </div>
-    <div class="divLong2 blanc" id="loisirs">
-        <div class="divLong2 blanc pad titre1">Centres d'intérêt</div>
-        <div style="margin-left : 6%">
-
-
-        </div>
-        <div class="divCentre">
-            <a href="#formation" class="scroll" title="Vers Formation"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBlancB-e1464822830732.jpg" 
+            <a href="#experience" class="scroll" title="Vers Experience"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBlancB-e1464822830732.jpg" 
                                                                               onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBlancBHover-e1464822821425.jpg'"
                                                                               onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBlancB-e1464822830732.jpg'"></a>
-            <a href="#telechargement" class="scroll" title="Vers Téléchargement"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+            <a href="#loisirs" class="scroll" title="Vers Loisirs"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
                                                                                       onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
                                                                                       onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 bleu" id="loisirs">
+        <div class="divLong2 bleu pad titre1">Centres d'intérêt</div>
+        <div style="margin-left : 6%">
+
+
+        </div>
+        <div class="divCentre">
+            <a href="#formation" class="scroll" title="Vers Formation"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
+                                                                              onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleuHover-e1464822751465.jpg'"
+                                                                              onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg'"></a>
+            <a href="#telechargement" class="scroll" title="Vers Téléchargement"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg" 
+                                                                                      onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleuHover-e1464822759168.jpg'"
+                                                                                      onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBleu-e1464822773139.jpg'"></a>
         </div>
     </div>
     <div class="divLong2 rouge" id="telechargement">
