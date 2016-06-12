95cc844dfc28f7f1001b0128ebf7743d165b5b3f
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-12 21:46:10 +0200

    accueil OK

---
+++ accueilHtml.php
@@ -0,0 +1,25 @@
+<div class="divLong blanc" style="margin-top : -120px"> 
+    <div class="divLong2 blanc">
+        <div class="divLong2 blanc pad titre1">Bonjour et bienvenue,</div>
+        <div class="imageGauche pad">
+            <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/moi.png" >
+        </div>
+        <div class="texteDroite" >
+            &nbsp;&nbsp;&nbsp;&nbsp;Je m’appelle <b>Audrey Guilbaud</b> et je suis actuellement en 2nd année de <b>BTS Services Informatiques aux Organisations</b> spécialité <b>Solutions Logicielles et Applications Métiers</b>.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;L’informatique est une activité qui me passionne depuis que j’ai posé les doigts sur un clavier. J’ai toujours cherché à savoir comment fonctionnait les choses ou comment les réparer. 
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;L’intérêt pour le développement informatique en particulier est venu un peu plus tard, quand j’ai commencé à créer des sites et blogs sur des <b>CMS hébergés</b> et que l’apparence et les thèmes ne me convenaient pas. J’ai donc commencé à fouiller dans les fichiers <b>CSS</b>, puis appris quelques notions de 
+            <b>HTML</b> et de fil en aiguille j’en suis arrivée à m’inscrire au <b>BTS SIO</b>.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <div class="divLong2 blanc pad">
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Je souhaite commencer à <b>travailler</b> le plus vite possible, si possible dans le domaine du développement Web. Néanmoins, je ne souhaite pas pour autant renier les études. Il existe plusieurs possibilités de <b>continuer les études</b> en travaillant (CNAM, cours du soir…) et, forte de mon expérience d’autonomie et d’autogestion au CNED, je compte bien en profiter.
+            <br>
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;En fait, je ne souhaite jamais réellement arrêter d’étudier et c’est bien pour ça que l’informatique est un métier qui me plait : il est <b>impossible de se reposer sur ses acquis</b> au vu de <b>l’impressionnante évolution</b> des métiers et il est essentiel de <b>toujours rester en apprentissage</b>.
+        </div>
+        <div class="divLong2 blanc pad" style="text-align : center" >
+            <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/phpelephant.png" >
+        </div>
+    </div>
+</div>
