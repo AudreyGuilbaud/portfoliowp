6bb21dc9686a2995f49acf6d3bc1402fadcb94f6
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-12 03:38:18 +0200

    projets personnels Ok

---
+++ ajoutCode/projetsHTML.php
@@ -0,0 +1,46 @@
+<div class="divLong blanc" id="situation1"> 
+    <div class="divLong2 bleu pad titre1" >Application "Calcul des notes au BTS" (PHP/Ajax)</div>
+    <div class="divLong2 blanc" id="contexte1">
+        <div class="divLong2 blanc pad " style="text-align : center">
+            <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/bannierenotesbts-e1465694823875.jpeg" >
+        </div>
+        <div class="divLong2 blanc ">
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Cette petite application Web permet de calculer la moyenne générale
+            obtenue au BTS SIO en renseignant chaque note dans chaque matière. Le calcul s'effectue ensuite automatiquement,
+            sans validation.
+            
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Bien que son utilisation ne soit pas absolument nécessaire, j'ai choisi d'utiliser
+            la technologie Ajax à des fins d'entraînement. Le calcul des notes se fait donc en PHP.
+            
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Le calcul de la moyenne se fait au fur et à mesure, il est donc possible de ne
+            renseigner que quelques notes.
+            
+        </div>
+        <div class="divCentre bas">
+            <a href="#telechargements1" class="scroll basBlancB" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+                                                                                                   onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
+                                                                                                   onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 rouge" id="telechargements1">
+        <div style="margin-left:6%;">
+
+        </div>
+        <div class="divCentre">
+            <ul class="menuReaR">
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/projetspersonnels/notesbts.zip">Téléchargement du contenu</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/notesBts/archive/master.zip">Lien alternatif</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/notesBts" target="_blank">GitHub</a></li>
+                <li class="menuReaR"><a href="http://calculnotes.audreyguilbaud.fr/" target="_blank">Application en ligne</a></li>
+            </ul>
+        </div>
+
+        <div class="divCentre">
+            <a href="#contexte1" class="scroll" title="Vers Contexte"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
+                                                                           onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRougeHover-e1464822716205.jpg'"
+                                                                           onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg'"></a>
+             </div>
+    </div>
+    
+</div>
+
