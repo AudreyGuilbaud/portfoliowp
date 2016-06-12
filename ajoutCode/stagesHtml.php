12f1c8c2d5c26839ff3617ca6c254e18fb97f2b0
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-12 19:48:21 +0200

    fiche stage Ok

--- ajoutCode/stagesHtml.php
+++ ajoutCode/stagesHtml.php
@@ -6,9 +6,175 @@
         </ul>
     </div>
 </div>
-<div class="divLong2 blanc" id="situation1"> 
-    test
+<div class="divLong blanc" id="situation1"> 
+    <div class="divLong2 blanc" id="contexte1">
+        <div style="margin-top : 30px" class="pad">
+            <div class="imageGauche" style="margin-right : 5%">
+                <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/logo-carsat.png" >
+            </div>
+
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;La Caisse d’Assurance Retraite et de la Santé Au Travail (CARSAT) est un organisme de la Sécurité 
+            Sociale. Il s’agit d’une structure de droit privé exerçant une mission de service public.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;La CARSAT intervient auprès des salariés, des retraités et des entreprises de la région. Elle possède trois missions principales :
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- la retraite, que ce soit auprès des salariés qui souhaitent préparer leur retraite ou 	le versement de leurs indemnités aux retraités ;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- l’action sociale, consistant à venir en aide aux personnes fragilisées ou 	handicapées ;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- la gestion des risques professionnels, que ce soit par la prévention auprès des 	entreprises et 	des salariés ou par le versement d’indemnités au titre d’exposition à 	un risque professionnel (exemple : l’amiante).
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;J’ai évolué au sein du service informatique de la CARSAT, plus particulièrement 
+            au pôle Support Développement. Le pôle Support Développement a pour mission d’apporter son aide aux développeurs 
+            d’autres CARSAT sur des technologies telles que Microsoft Sharepoint. 
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Ils développent également  des applications basées sur la technologie Sharepoint pour la CARSAT des Pays de la Loire.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Un service spécifique de la CARSAT, le Service Pilotage de la Performance, a requis le développement d’un petit applicatif afin d’informatiser les demandes qui leur sont faites.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+
+        <div class="divCentre bas">
+            <a href="#telechargements1" class="scroll basBlancB" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+                                                                                                   onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
+                                                                                                   onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+        </div>      
+    </div>
+    <div class="divLong2 rouge" id="telechargements1">
+        <div style="margin-left:6%;">
+            <br/>Le dossier de documents ci-dessous contient :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le cahier des charges du client ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le compte-rendu de la réunion avec le client ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les premières maquettes du formulaire proposées ;          
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Les spécifications fonctionnelles générales ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le dossier de sécurité ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-Le dossier de tests ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La veille sur Sharepoint ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;-La veille sur Infopath.
+            <br/>
+        </div>
+        <div class="divCentre">
+            <ul class="menuReaR">
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/stage1/stage1.zip">Téléchargement des documents</a></li> 
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/stage1/rapportStage1ereannee.docx">Téléchargement du rapport de stage</a></li> 
+            </ul>
+        </div>
+
+        <div class="divCentre">
+            <a href="#contexte1" class="scroll" title="Vers Contexte"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
+                                                                           onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRougeHover-e1464822716205.jpg'"
+                                                                           onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg'"></a>
+            <a href="#competences1" class="scroll" title="Vers Compétences"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg" 
+                                                                                 onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRougeHover-e1464822726938.jpg'"
+                                                                                 onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 bleu" id="competences1">
+        <div style="margin-left : 6%">
+            <br>
+            <br/>Situations obligatoires :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Participation à un projet d'évolution d'un SI (solution applicative et d'infrastructure portant prioritairement sur le domaine de spécialité du candidat ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Prise en charge d'incidents et de démarches d'assistance liées au domaine de spécialité du candidat.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Elaboration de documents relatifs à la production et à la fourniture de services ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Productions relatives à la mise en place d’un dispositif de veille technologique et à l’étude d’une technologie, d’un composant, d’un outil ou d’une méthode.
+            <br/>
+            <br/>Compétences mises en oeuvres :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.1.1</b> - Analyse du cahier des charges d’un service à produire ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.1.3</b> - Etude des exigences liée à la qualité attendue d'un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.2.1</b> - Elaboration et présentation d'un dossier de choix de solution technique ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.2.2</b> - Rédaction des spécifications techniques de la solution retenue ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.2.4</b> - Détermination des tests nécessaires à la validation d’un service ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.2.5</b> - Définition des niveaux d'habilitation associés à un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.4.1</b> - Participation à un projet ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.1.1</b> - Accompagnement des utilisateurs dans la prise en main d'un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.2.1</b> - Suivi et résolution d’incidents ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.1</b> - Identification, qualification et évaluation d’un problème ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.1</b> - Proposition d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.2</b> - Conception ou adaptation de l'interface utilisateur d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.4</b> - Définition des caratéristiques d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.5</b> - Prototypage de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.6</b> - Gestion d'environnement de développement et de tests ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.7</b> - Développement, utilisation ou adaptation de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.8</b> - Réalisation des tests nécessaires à la validation d’éléments adaptés ou développés ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.9</b> - Rédaction d’une documentation technique.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.1</b> - Exploitation des référentiels, normes et standards adoptés par le prestataire.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.2</b> - Veille technologique ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.3</b> - Repérage des compléments de formation, d'autoformation ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.4</b> - Etude d'une technologie, d'un composant, d'un outil, d'une méthode.
+        </div>
+        <div class="divCentre">
+            <a href="#telechargements1" class="scroll" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
+                                                                                         onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleuHover-e1464822751465.jpg'"
+                                                                                         onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg'"></a>
+        </div>
+    </div>
 </div>
-<div hidden class="divLong2 blanc" id="situation2"> 
-    test2
+<div hidden class="divLong blanc" id="situation2">
+    <div class="divLong2 blanc" id="contexte2">
+        <div style="margin-top : 30px" class="pad">
+            <div class="imageGauche" style="margin-right : 5%">
+                <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/logo.png" >
+            </div>
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Win Novation Informatique est une SARL dont la création date de 1995. Son activité principale est la programmation informatique.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;Les activités de Win Novation Informatique couvrent :
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- La création et l’hébergement de sites internet ;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- La création d’applications de gestion ;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- La maintenance et le dépannage des applications.
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Win Novation Informatique souhaite réaliser une version Web de son application de gestion d’un comité d’entreprise. 
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Le travail à réaliser part donc d’un existant : l’application initiale a été codée en C++ et sa base de données est sous Paradox. 
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Le projet de Win Novation Informatique est de réaliser cette application en version Web, en PHP, avec une base de données sous MySQL.
+
+        </div>
+        <div class="divCentre bas">
+            <a href="#telechargements2" class="scroll basBlancB" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+                                                                                                   onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
+                                                                                                   onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 rouge" id="telechargements2">
+        <div style="margin-left:6%;">
+            <br>
+        </div>
+        <div class="divCentre">
+            <ul class="menuReaR">
+                <li class="menuReaR"><a download href="http://telechargements.audreyguilbaud.fr/stage2/rapportStage2ndAnnee.pdf">Téléchargement du rapport de stage</a></li>  
+            </ul>
+        </div>
+
+        <div class="divCentre">
+            <a href="#contexte2" class="scroll" title="Vers Contexte"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
+                                                                           onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRougeHover-e1464822716205.jpg'"
+                                                                           onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg'"></a>
+            <a href="#competences2" class="scroll" title="Vers Compétences"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg" 
+                                                                                 onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRougeHover-e1464822726938.jpg'"
+                                                                                 onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 bleu" id="competences2">
+        <div style="margin-left : 6%">
+            <br>
+            <br/>Situations obligatoires :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Participation à un projet d'évolution d'un SI (solution applicative et d'infrastructure portant prioritairement sur le domaine de spécialité du candidat ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Prise en charge d'incidents et de démarches d'assistance liées au domaine de spécialité du candidat.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Productions relatives à la mise en place d’un dispositif de veille technologique et à l’étude d’une technologie, d’un composant, d’un outil ou d’une méthode.
+            <br/>
+            <br/>Compétences mises en oeuvres :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.1</b> - Identification, qualification et évaluation d’un problème ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.2</b> - Proposition d'amélioration d'un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.1</b> - Proposition d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.2</b> - Conception ou adaptation de l'interface utilisateur d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.3</b> - Conception ou adaptation d'une base de données ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.4</b> - Définition des caratéristiques d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.5</b> - Prototypage de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.6</b> - Gestion d'environnement de développement et de tests ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.7</b> - Développement, utilisation ou adaptation de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.8</b> - Réalisation des tests nécessaires à la validation d’éléments adaptés ou développés ; 
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.9</b> - Rédaction d’une documentation technique ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.3</b> - Repérage des compléments de formation, d'autoformation ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.4</b> - Etude d'une technologie, d'un composant, d'un outil, d'une méthode.
+        </div>
+        <div class="divCentre">
+            <a href="#telechargements2" class="scroll" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
+                                                                                         onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleuHover-e1464822751465.jpg'"
+                                                                                         onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg'"></a>
+        </div>
+    </div>
 </div>
