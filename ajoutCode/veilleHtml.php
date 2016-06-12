e13e610765939d18efe57611d14a3e7b0ea26404
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-12 20:45:30 +0200

    veille ok

---
+++ ajoutCode/veilleHtml.php
@@ -0,0 +1,34 @@
+<div class="divLong blanc" id="situation1" style="margin-top:-100px">
+    <div class="divLong2 rouge titre1">Veille technologique</div>
+    <div class="divLong2 blanc pad" id="contexte1">
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;<b>Qu’est-ce que la veille technologique ?</b>
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;La veille technologique est un élément essentiel dans le 
+        travail de l’informaticien. L’informatique est un domaine qui évolue très vite, il est 
+        alors de la responsabilité de chaque informaticien de ne pas rester sur ses acquis et 
+        de se tenir au courant des nouvelles avancées technologiques, particulièrement lorsqu’elles 
+        concernent son domaine.
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;Il faut alors mettre en place diverses techniques de recherche et 
+        de collecte des informations.
+    </div>
+    <div class="divLong2 bleu pad" id="contexte1">
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;Afin de me tenir globalement au courant des nouvelles avancées, j’utilise principalement les 
+        flux RSS. J’utilise le lecteur de flux en ligne gratuit et libre de framasoft : framanews.
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+        <ul class="menuReaB">
+            <li class="menuReaB"><a href="https://framanews.org/ttrss/#f=-3&c=0">Framanews</a></li>
+        </ul>
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;J’achète également régulièrement certaines revues professionnelles.
+         <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+        <ul class="menuReaB">
+            <li class="menuReaB"><a href="http://www.linformaticien.com/">L'Informaticien</a></li>
+            <li class="menuReaB"><a href="http://www.programmez.com/">Programmez !</a></li>
+        </ul>
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;J’utilise également Pearltrees afin de  centraliser et de classer mes recherches. Je m’intéresse particulièrement 
+        aux technologies touchant le développement Web et le développement mobile.
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+        <br/>&nbsp;&nbsp;&nbsp;&nbsp;<iframe src="//www.pearltrees.com/annaido?embed=2&d=201606122031" width="1000" height="400" style="border:0px;" allowtransparency="true"></iframe><span style="display:block; padding-top:2px; color:#818181; font-size:13px;"><a href="http://www.pearltrees.com/annaido" style=" color:#818181; font-size:13px;" target="_blank">Audrey Guilbaud</a></span>
+    </div>
+</div> 
+
