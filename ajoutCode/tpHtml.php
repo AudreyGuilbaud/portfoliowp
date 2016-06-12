5b969d8eb4ffe14807a98207072d67d14d679c1f
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-06-12 03:23:46 +0200

    partie tp Ok

---
+++ ajoutCode/tpHtml.php
@@ -0,0 +1,78 @@
+<div class="divLong blanc" id="situation1">
+    <div class="divLong2 bleu pad titre1">Application "Coach" (Android - Java - SqlLite)</div>
+    <div class="divLong2 blanc" id="contexte1">
+        <div class="imageGauche">
+            <img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/appli-Android-e1465692995315.png" >
+        </div>
+        <div class="texteDroite" >
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;L'application "Coach" est une petite application développée lors d'un TP 
+            concernant la programmation sous Android.
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;Elle a été réalisée en Java à l'aide l'IDE Android Studio.
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;Elle révèle à l'utilisateur son IMC (Indice de Masse Corporelle) à partir 
+            des trois renseignements que celui-ci indique à l'application : son âge, son poids et sa taille.
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;L'application sauvegarde les résultats précédents et affiche un historique.
+            SQL Lite a été utilisé à cet effet.
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;
+            <br><br>&nbsp;&nbsp;&nbsp;&nbsp;
+            
+        </div>
+        <div class="divCentre bas">
+            <a href="#telechargements1" class="scroll basBlancB" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg" 
+                                                                                                   onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancBHover-e1464822040880.jpg'"
+                                                                                                   onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basBlancB-e1464822407938.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 rouge" id="telechargements1">
+        <div style="margin-left:6%;">
+
+        </div>
+        <div class="divCentre">
+            <ul class="menuReaR">
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/tp/androidCoach.zip">Téléchargement du contenu</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/androidCoach/archive/master.zip">Lien alternatif</a></li> 
+                <li class="menuReaR"><a href="https://github.com/AudreyGuilbaud/androidCoach" target="_blank">GitHub</a></li> 
+                <li class="menuReaR"><a href="http://telechargements.audreyguilbaud.fr/tp/coach.apk">APK</a></li> 
+            </ul>
+        </div>
+
+        <div class="divCentre">
+            <a href="#contexte1" class="scroll" title="Vers Contexte"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg" 
+                                                                           onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRougeHover-e1464822716205.jpg'"
+                                                                           onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautRouge-e1464822735469.jpg'"></a>
+            <a href="#competences1" class="scroll" title="Vers Compétences"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg" 
+                                                                                 onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRougeHover-e1464822726938.jpg'"
+                                                                                 onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/basRouge-e1464822744589.jpg'"></a>
+        </div>
+    </div>
+    <div class="divLong2 bleu" id="competences1">
+        <div style="margin-left : 6%">
+            <br>
+            <br/>Situations obligatoires :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Participation à un projet d'évolution d'un SI (solution applicative et d'infrastructure portant prioritairement sur le domaine de spécialité du candidat ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; -Prise en charge d'incidents et de démarches d'assistance liées au domaine de spécialité du candidat.
+            <br/>
+            <br/>Compétences mises en oeuvres :
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A1.3.4</b> - Déploiement d'un service ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A2.3.1</b> - Identification, qualification et évaluation d'un problème ;           
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.1</b> - Proposition d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.2</b> - Conception ou adaptation de l'interface utilisateur d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.3</b> - Conception ou adaptation d'une base de données ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.4</b> - Définition des caratéristiques d'une solution applicative ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.5</b> - Prototypage de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.6</b> - Gestion d'environnement de développement et de tests ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.1.7</b> - Développement, utilisation ou adaptation de composants logiciels ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A4.2.2</b> - Adaptation d'une solution applicative aux évolutions de ses composants ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.3</b> - Repérage des compléments de formation ou d'auto-formation ;
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp; <b>A5.2.4</b> - Étude d˜une technologie, d'un composant, d'un outil ou d'une méthode.
+            <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <div class="divCentre">
+            <a href="#telechargements1" class="scroll" title="Vers Téléchargements"><img src="<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg" 
+                                                                                         onMouseOver="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleuHover-e1464822751465.jpg'"
+                                                                                         onMouseOut="this.src = '<?php echo bloginfo('url'); ?>/wp-content/uploads/2016/06/hautBleu-e1464822766413.jpg'"></a>
+        </div>
+    </div>
+</div>
